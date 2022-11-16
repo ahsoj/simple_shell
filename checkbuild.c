11a2de4ddb8ab26f9b4380e65b64eb4e1897618b
ahsoj <eyasuchaka259@gmail.com>
2022-11-16 04:20:35 -0800

    end-task

---
+++ checkbuild.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+
+/**
+ * checkbuild - checks if the command is buildin
+ * @arv: array of arguments
+ *
+ * Return: pointer to function that arv and returns void
+ */
+
+void(*checkbuild(char **arv))(char **arv)
+{
+	int i, j;
+	mybuild T[] = {
+		{"exit", exitt},
+		{"env", env},
+		{"setenv", _setenv},
+		{"unsetenv", _unsetenv},
+		{NULL, NULL}
+	};
+
+	for (i = 0; T[i].name; i++)
+	{
+		j = 0;
+		if (T[i].name[j] == arv[0][j])
+		{
+			for (j = 0;arv[0][j]; j++)
+			{
+				if (T[i].name[j] != arv[0][j])
+					break;
+			}
+			if (!arv[0][j])
+				return (T[i].func);
+		}
+	}
+	return (0);
+}
